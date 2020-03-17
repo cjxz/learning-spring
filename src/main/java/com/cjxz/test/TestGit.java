33b3fe941ded06fb6d1a3c6000756c219ec834d1
zhuchao2 <zhuchao2@tuhu2.cm>
2020-03-17 21:00:10 +0800

    添加git

---
+++ src/main/java/com/cjxz/test/TestGit.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2020 jv.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with jv.cn
+ */
+package com.cjxz.test;
+
+/**
+ * @author zhuchao
+ * @date 2020/3/17下午8:44
+ */
+public class TestGit {
+}
